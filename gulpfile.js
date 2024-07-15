f1195adfe99b356a26e23e9866e00741d15f24bd
Fernando Léon <ferzleon51@gmail.com>
2024-07-15 10:34:05 -0600

    correcciones al gulpfile y cambios en el package.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ import gulp from 'gulp';
 const { src, watch, parallel } = gulp;
 
 const paths = {
-   sassFiles: './src/scss/**/*.scss',
-   cssDest: './build/css',
-   imageFiles: './src/assets/img/**/*',
-   imageDest: './build/assets/img',
-   jsFiles: './src/js/**/*.js',
-   jsBundle: './build/js',
+   styles: {
+    src: './src/scss/**/*.scss',
+    dest: './build/css'
+  },
+  scripts: {
+   src: './src/js/**/*.js',
+   dest: './build/js'
+ },
+  images: {
+   src: './src/assets/img/**/*',
+   dest: './build/assets/img'
+ }
 };
 
 // CSS
@@ -19,12 +25,6 @@ import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
 import purgecss from 'gulp-purgecss';
 
-// Imagenes
-import cache from 'gulp-cache';
-import imagemin from 'gulp-imagemin';
-import webp from 'gulp-webp';
-import avif from 'gulp-avif';
-
 // Javascript
 import terser from 'gulp-terser';
 
@@ -32,25 +32,25 @@ import terser from 'gulp-terser';
 const sassCompiler = sass(dartSass);
 
 // Tareas
-function css(done) {
+export function css(done) {
    // sass directory
-   src(paths.sassFiles)
+   src(paths.styles.src)
       // sourcemaps
       .pipe(sourcemaps.init())
       .pipe(plumber())
       // outputstyle (nested, compact, expanded, compressed)
-      .pipe(sassCompiler({ outputStyle: 'compressed' }))
+      .pipe(sassCompiler({ outputStyle: 'compressed' })/* .on('error', sass.logError) */)
       // autoprefixer
       .pipe(postcss([autoprefixer({
          overrideBrowserslist: ['> .5%, last 10 versions']
       })]))
       .pipe(sourcemaps.write('./'))
       // css output directory
-      .pipe(gulp.dest(paths.cssDest));
+      .pipe(gulp.dest(paths.styles.dest));
    done();
 };
 
-function cleanCSS(done) {
+export function cleanCSS(done) {
    src('build/**/*.css')
       .pipe(purgecss({
          content: ['./*.html'],
@@ -60,85 +60,19 @@ function cleanCSS(done) {
    done();
 };
 
-function images(done) {
-   src(paths.imageFiles)
-      .pipe(cache(imagemin([
-         imagemin.gifsicle({
-            interlaced: true
-         }),
-         imagemin.mozjpeg({
-            quality: 50,
-            progressive: true
-         }),
-         imagemin.optipng({
-            optimizationLevel: 3
-         }),
-         imagemin.svgo({
-            plugins: [{
-                  removeViewBox: true
-               },
-               {
-                  cleanupIDs: false
-               }
-            ]
-         })
-      ])))
-      .pipe(gulp.dest(paths.imageDest));
-   done();
-};
-
-function avifImages(done) {
-   const opciones = {
-      quality: 50
-   };
-   src('src/assets/img/**/*.{png,jpg}')
-      .pipe(avif(opciones))
-      .pipe(gulp.dest(paths.imageDest))
-   done();
-};
-
-function webpImages(done) {
-   src(paths.imageFiles)
-      .pipe(cache(imagemin([
-         imagemin.gifsicle({
-            interlaced: true
-         }),
-         imagemin.mozjpeg({
-            quality: 50,
-            progressive: true
-         }),
-         imagemin.optipng({
-            optimizationLevel: 3
-         }),
-         imagemin.svgo({
-            plugins: [{
-                  removeViewBox: true
-               },
-               {
-                  cleanupIDs: false
-               }
-            ]
-         })
-      ])))
-      .pipe(webp())
-      .pipe(gulp.dest(paths.imageDest));
-   done();
-};
-
-function js(done) {
-   src(paths.jsFiles)
+export function js(done) {
+   src(paths.scripts.src)
       .pipe(sourcemaps.init())
       .pipe(terser())
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(paths.jsBundle));
+      .pipe(gulp.dest(paths.scripts.dest));
    done();
 };
 
-function dev(done) {
-   watch(paths.sassFiles, css);
-   watch(paths.jsFiles, js);
+ export function dev(done) {
+   watch(paths.styles.src, css);
+   watch(paths.scripts.src, js);
    done();
 }
 
-const build = parallel(images, webpImages, avifImages, dev);
-export { css, cleanCSS, js, images, webpImages as webp, avifImages as avif, build as dev };
+// const build = parallel(images, webpImages, avifImages, dev);
