63bc3bb7b542ba7b42bc1661516f192fac8db0a2
Fernando Léon <ferzleon51@gmail.com>
2024-07-17 12:04:41 -0600

    finalizacion del proyecto, se añadio para proccesar las imagenes con gulp y convertirlas a avif y webp, se hicieron algunos cambios en la estructura de salida de los archivos, se añadieron imagenes de la nueva version del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 import gulp from 'gulp';
 const { src, watch, parallel } = gulp;
+import path from 'path'
+import fs from 'fs'
+import { glob } from 'glob'
+import sharp from 'sharp'
 
 const paths = {
    styles: {
@@ -68,6 +72,63 @@ export function js(done) {
       .pipe(gulp.dest(paths.scripts.dest));
    done();
 };
+
+export async function crop(done) {
+   const inputFolder = 'src/assets/img/gallery/full'
+   const outputFolder = 'src/assets/img/gallery/thumb';
+   const width = 250;
+   const height = 180;
+   if (!fs.existsSync(outputFolder)) {
+       fs.mkdirSync(outputFolder, { recursive: true })
+   }
+   const images = fs.readdirSync(inputFolder).filter(file => {
+       return /\.(jpg)$/i.test(path.extname(file));
+   });
+   try {
+       images.forEach(file => {
+           const inputFile = path.join(inputFolder, file)
+           const outputFile = path.join(outputFolder, file)
+           sharp(inputFile) 
+               .resize(width, height, {
+                   position: 'centre'
+               })
+               .toFile(outputFile)
+       });
+
+       done()
+   } catch (error) {
+       console.log(error)
+   }
+}
+
+export async function images(done) {
+   const srcDir = './src/assets/img';
+   const buildDir = './build/img';
+   const images =  await glob('./src/assets/img/**/*{jpg,png}')
+
+   images.forEach(file => {
+       const relativePath = path.relative(srcDir, path.dirname(file));
+       const outputSubDir = path.join(buildDir, relativePath);
+       procesarImagenes(file, outputSubDir);
+   });
+   done();
+}
+
+function procesarImagenes(file, outputSubDir) {
+   if (!fs.existsSync(outputSubDir)) {
+       fs.mkdirSync(outputSubDir, { recursive: true })
+   }
+   const baseName = path.basename(file, path.extname(file))
+   const extName = path.extname(file)
+   const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+   const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+   const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+
+   const options = { quality: 80 }
+   sharp(file).jpeg(options).toFile(outputFile)
+   sharp(file).webp(options).toFile(outputFileWebp)
+   sharp(file).avif().toFile(outputFileAvif)
+}
 
  export function dev(done) {
    watch(paths.styles.src, css);
